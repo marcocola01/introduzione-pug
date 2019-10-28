b8966ed8b1952be7ceb5a0686d37d86a0c73c4fc
marcocola01 <colangelo.marco@iisgalvanimi.edu.it>
2019-10-28 09:57:34 +0000

    esercizio 02.1 finito

---
+++ introduzione-pug/ESEMPIO_PUG/app.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const people = require('./people.json');
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles
+  });
+});
+
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
+});
+
